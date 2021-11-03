7ebd223b57d4677fc9347705ceac3daa0ab85f38
Mauricio <mlsilva1990@gmail.com>
2021-11-02 22:35:15 -0300

    02/11 comit

---
+++ calculadora.py
@@ -0,0 +1,39 @@
+#
+def adicao(val1, val2):
+    return val1 + val2
+
+def subtracao(val1, val2):
+    return val1 - val2
+
+def multiplicacao(val1, val2):
+    return val1 * val2
+
+def divisao(val1, val2):
+    return val1 // val2
+
+print("Selecione o número da operação desejada: \n ")
+
+print("1 - Soma")
+print("2 - Subtração")
+print("3 - Multiplicação")
+print("4 - Divisão")
+
+opcao = int(input("\nDigite a sua opção (1/2/3/4): "))
+
+if opcao <= 0 or opcao > 4:
+    print("\nOpção inválida!\n")
+
+val1 = int(input("\nDigite o primeiro número: "))
+val2 = int(input("\nDigite o segundo número: "))
+
+if opcao == 1:
+    print(val1, "+",  val2, "=", adicao(val1, val2))
+
+elif opcao == 2:
+    print(val1, "-",  val2, "=", subtracao(val1, val2))
+
+elif opcao == 3:
+    print(val1, "*",  val2, "=", multiplicacao(val1, val2))
+
+elif opcao == 4:
+    print(val1, "/",  val2, "=", divisao(val1, val2))
---
+++ desafio_1.py
@@ -0,0 +1,20 @@
+# conte a quantidade de letras no texto e também a quantidade de letras repetidas.
+
+# # loop exemplo 
+# palavra = 'Matemática'
+# for i in palavra:
+#     print(i)
+
+# fruta = ["maçã", "banana", "abacaxi"]
+# #print(fruta)
+# #len(fruta)
+# for x in fruta:
+#     print(len)
+
+# fruta = "Banana"
+# print(len(fruta))
+
+# loop exemplo 
+palavra = 'Matemática'
+for i in palavra:
+    print(len(palavra))
